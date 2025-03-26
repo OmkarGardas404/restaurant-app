9c950966e238d69fe28e49684e2d841a09eff6dd
Omkar Gardas <omkar_gardas@epam.com>
2025-03-26 17:31:41 +0530

    folder structure created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-xl'>Hello</h1>
+    <Button variant="destructive" className='text-black'>click me</Button>
+    </>
+  )
+}
+
+export default App
